979d213e13ef6953aae85b6e4be40f49b46f320f
naufalzhafran <latifnaufal7@gmail.com>
2020-03-15 16:23:14 +0700

    add rents migration table

---
+++ database/migrations/2020_03_15_091224__table_rents_.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TableRents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('cd_id');
+            $table->foreign('cd_id')->references('id')->on('cds');
+            $table->date('borrow_date');
+            $table->date('return_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rents');
+    }
+}
