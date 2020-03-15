ea72b1935b2763d374b06377e2ae66b027efb44c
naufalzhafran <latifnaufal7@gmail.com>
2020-03-15 18:37:11 +0700

    Change model file name

---
+++ app/Http/Controllers/CDController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CD;
+
+class CDController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
